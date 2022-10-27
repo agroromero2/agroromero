932351fae46735af98b61db275a2d2df6f8e47ad
Leidy <leidyhurtado-1@hotmail.com>
2022-10-27 13:24:12 -0400

    Interfaz para visualizacion de productos

---
+++ src/Componentes/Productos/Dproductos.js
@@ -0,0 +1,48 @@
+import { Children } from "react";
+import Lproductos from "./Lproductos";
+import Vistaproductos from "./Vistaproductos";
+
+export const Dproductos =[
+  {
+      id: 1,
+      nombre: "Fitoraz",
+      cantidad: 15,
+      precio: 26.000,
+      unidad: "500 gr",
+      imagen: "../../img/imagen 1.jpg",
+  },
+  {
+      id: 2,
+      nombre: "Forum",
+      cantidad: 10,
+      precio: 16.000,
+      unidad: "120 gr",
+      imagen: "../../img/imagen 1.jpg",
+  },
+  {
+      id: 3,
+      nombre: "Glifosol",
+      cantidad: 8,
+      precio: 36.000,
+      unidad: "Lt",
+      imagen: "../../img/imagen 1.jpg",
+  },
+  {
+      id: 4,
+      nombre: "Propamocar",
+      cantidad: 5,
+      precio: 50.000,
+      unidad: "lt",
+      imagen: "../../img/imagen 1.jpg",
+  },
+  {
+      id: 5,
+      nombre: "Cayenne",
+      cantidad: 7,
+      precio: 165.000,
+      unidad: "lt",
+      imagen: "../../img/imagen 1.jpg",
+  }
+
+]; 
+
